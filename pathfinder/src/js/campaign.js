0be088371d7065b7ab38bd12d29aa702bd4a283e
Chris Harty <ascii1011@gmai.com>
2014-04-16 23:52:48 -0400

    updated campaign.js with some buttons.  now adding click events.

--- pathfinder/src/js/campaign.js
+++ pathfinder/src/js/campaign.js
@@ -1,37 +1,47 @@
-$.ajax({ 
-    type: 'GET', 
-    url: '/campaign/list/', 
-    data: {}, 
-    dataType: 'json',
-    success: function (data) { 
-        $.each(data, function(index, element) {
-            var onoff = '';
-            if (element.onoff == 1) { 
-                onoff = 'Stop';
-            } else {
-                onoff = 'Start';
-            }
-            $('#camp_list').append(
-                $('<li>').append( 
-                    $('<a>').attr('href',element.slug+'/').text(element.name)
-                )
-            );
-            $('#camp_list_div').append(
-                $('<tr>').append(
-                    $('<td>').append(
+
+
+var def_campaign_list = function() {
+    $.ajax({ 
+        type: 'GET', 
+        url: '/campaign/list/', 
+        data: {}, 
+        dataType: 'json',
+        success: function (data) { 
+            $.each(data, function(index, element) {
+
+                var onoff = '';
+                if (element.onoff == 1) { 
+                    onoff = 'Stop';
+                } else {
+                    onoff = 'Start';
+                }
+                var btn_campaign = $('<button class="campaignLink">'+element.name+'</button>');
+                var btn_remove = $('<button class="removeLink">Remove</button>');
+                var btn_onoff = $('<button class="onoffLink">'+onoff+'</button>');
+
+                $('#camp_list').append(
+                    $('<li>').append( 
                         $('<a>').attr('href',element.slug+'/').text(element.name)
                     )
-                ).append(
-                    $('<td>').append(
-                        $('<a>').attr('class','removeLink').attr('href','/del/'+element.slug+'/').text('remove')
+                );
+                $('#camp_list_div').append(
+                    $('<tr>').append(
+                        $('<td>').append(
+                            $('<a>').attr('href',element.slug+'/').text(element.name)
+                        )
                     ).append(
-                        $('<a>').attr('class','onoffLink').attr('href','/onoff/'+element.slug+'/').text(onoff)
+                        $('<td>').append(
+                            btn_remove
+                        ).append(
+                            btn_onoff
+                        )
+                    ).append(
+                        $('<td>').text(element.status)
                     )
-                ).append(
-                    $('<td>').text(element.status)
-                )
-                        
-            );
-        });
-    }
-});
+                    
+                );
+            });
+        }
+    });
+    
+};
