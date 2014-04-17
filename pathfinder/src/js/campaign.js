57425021b7e271a0ffd7157e5a503c1c2dafeb18
Chris Harty <ascii1011@gmai.com>
2014-04-16 23:34:25 -0400

    updated js and css for campaign for remove and onoff ajax calls, but realized I wanted to do it with onclick events... less visible clutter

--- pathfinder/src/js/campaign.js
+++ pathfinder/src/js/campaign.js
@@ -5,10 +5,32 @@ $.ajax({
     dataType: 'json',
     success: function (data) { 
         $.each(data, function(index, element) {
+            var onoff = '';
+            if (element.onoff == 1) { 
+                onoff = 'Stop';
+            } else {
+                onoff = 'Start';
+            }
             $('#camp_list').append(
                 $('<li>').append( 
                     $('<a>').attr('href',element.slug+'/').text(element.name)
                 )
+            );
+            $('#camp_list_div').append(
+                $('<tr>').append(
+                    $('<td>').append(
+                        $('<a>').attr('href',element.slug+'/').text(element.name)
+                    )
+                ).append(
+                    $('<td>').append(
+                        $('<a>').attr('class','removeLink').attr('href','/del/'+element.slug+'/').text('remove')
+                    ).append(
+                        $('<a>').attr('class','onoffLink').attr('href','/onoff/'+element.slug+'/').text(onoff)
+                    )
+                ).append(
+                    $('<td>').text(element.status)
+                )
+                        
             );
         });
     }
